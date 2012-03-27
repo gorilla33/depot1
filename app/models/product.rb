786b83deaa3fbb66b7c685ba5b625342cd9353dc
Yury Azarenok <gorilla33@mail.ru>
2012-03-27 20:25:29 +0300

    Iteration B1 (without unit testing)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ActiveRecord::Base
+	#проверка заполнения полей
+	validates :title, :description, :image_url, :presence => true
+
+	#проверка , что цена больше 1 цента
+	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+
+	#проверка, что название книги не дублируется
+	validates :title, :uniqueness => true
+
+	#проверка, что URL картинки книги имеет правильный формат
+	validates :image_url, :format => {
+		:with => %r{\.(gif|jpg|png)$}i , 
+		:message => 'must be a URL for GIF, JPG or PNG image.'
+	}
+
 end
