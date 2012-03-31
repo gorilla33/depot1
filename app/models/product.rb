ef3a1023badc40f1497bd9a17d5d723e319fe246
Yury Azarenok <gorilla33@mail.ru>
2012-03-31 21:49:36 +0300

    Iteration B playtime

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,12 +6,15 @@ class Product < ActiveRecord::Base
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 
 	#проверка, что название книги не дублируется
-	validates :title, :uniqueness => true
+	validates :title, :uniqueness => true, :length=>{
+							:minimum=>10,
+							:message=>:too_short}
 
 	#проверка, что URL картинки книги имеет правильный формат
 	validates :image_url, :format => {
 		:with => %r{\.(gif|jpg|png)$}i , 
 		:message => 'must be a URL for GIF, JPG or PNG image.'
 	}
+
 
 end
